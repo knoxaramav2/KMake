7c5ad000f29531d3a0595aeacb1868690aa07a0c
knoxaramav2 <wtscott@iastate.edu>
2020-03-16 03:01:17 -0500

    CLI options

--- crossplat.cpp
+++ crossplat.cpp
@@ -11,6 +11,13 @@ vector <string> getEnvVar(){
     
     const char * envPath = std::getenv("PATH");
     
+    int8_t a =3;
+    uint8_t b = 4;
+
+    if (a<b){
+        return paths;
+    }
+
 
     return paths;
 }
